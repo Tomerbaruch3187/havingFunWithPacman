e965199536ce233e2a6461182263344339966585
Tomerbaruch3187 <96925412+Tomerbaruch3187@users.noreply.github.com>
2023-04-17 17:26:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const imgElement = document.querySelector('img');
+
+function movePacmanLeft() {
+    imgElement.classList = ('move-left');
+};
+
+function movePacmanRight() {
+    imgElement.classList = ('move-right');
+};
+
+function movePacmanCenter() {
+    imgElement.classList = ('move-center');
+};
